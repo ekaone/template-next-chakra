64942ca9fe1e05f1cac09b7d07fbfafa205ee008
Eka Prasetia <ekaone3033@gmail.com>
2023-04-11 21:52:23 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,43 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { Montserrat, Chivo } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: "400",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+const chivo = Chivo({
+  weight: "400",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  gradients: {
+    "gradient-1": "linear-gradient(65deg, #7928CA 10%, #FF0080 100%)",
+  },
+  fonts: {
+    montserrat: montserrat.style.fontFamily,
+    chivo: chivo.style.fontFamily,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,61 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Curiosity rover, read widely, try new things."
+        />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemProp="name" content="Curiosity Rover" />
+        <meta
+          property="og:site_name"
+          content="https://curiosityrover.vercel.app/"
+        />
+        <meta
+          property="og:description"
+          content="Curiosity rover, read widely, try new things."
+        />
+        <meta
+          itemProp="image"
+          content="https://curiosityrover.vercel.app/og-image.png"
+        />
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://curiosityrover.vercel.app/" />
+        <meta property="og:type" content="Curiosity Rover" />
+        <meta property="og:title" content="Curiosity Rover" />
+        <meta
+          property="og:description"
+          content="Curiosity rover, read widely, try new things."
+        />
+        <meta
+          property="og:image"
+          content="https://curiosityrover.vercel.app/og-image.png"
+        />
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Curiosity Rover" />
+        <meta
+          name="twitter:description"
+          content="Curiosity rover, read widely, try new things."
+        />
+        <meta
+          property="og:image"
+          content="https://curiosityrover.vercel.app/og-image.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://curiosityrover.vercel.app/og-image.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
